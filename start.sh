601b2048ec64e1fd923628c69de999adef2fcfff
akrcreation2 <88943345+akrcreation2@users.noreply.github.com>
2022-12-03 20:57:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/akrcreation2/LazyPrincess.git /Knmlpro2
+  git clone https://github.com/akrcreation2/KNMLPRO2.git /Knmlpro2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Knmlpro2
 fi
-cd /LazyPrincess
+cd /Knmlpro2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
